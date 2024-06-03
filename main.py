1c1622a31025b78c7aabca57d8f10e54f260a480
VamsiVarma <vamsi.hansi@gmail.com>
2024-06-03 12:54:07 +0530

    Data ingestion added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-#from src.EndToEndChestCancerClassification import logger
+from EndToEndChestCancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from EndToEndChestCancerClassification import logger
 
-logger.info("Welcome to EndToEndChestCancerClassification!")
+STAGE_NAME = "Data Ingestion stage"
 
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
