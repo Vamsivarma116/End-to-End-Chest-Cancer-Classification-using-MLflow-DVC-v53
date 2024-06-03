5097f00f35d75a05bec675a36c2eb9126112213c
VamsiVarma <vamsi.hansi@gmail.com>
2024-06-04 05:25:08 +0530

    Model trainer added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from EndToEndChestCancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from EndToEndChestCancerClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from EndToEndChestCancerClassification.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from EndToEndChestCancerClassification import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -25,3 +26,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
